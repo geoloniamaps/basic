b1763f931291cdc6a4e65d061418e4a8fa4b7865
naoki <n.globe.us@gmail.com>
2022-08-12 14:41:57 +0900

    fix comment format

--- bin/style-render-time.js
+++ bin/style-render-time.js
@@ -41,7 +41,7 @@ const getAverageMapRenderTime = async (zoom, center) => {
   const mapRenderedTimes = [];
 
   // 5回のレンダリング時間の平均を取得
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     const mapRenderedTime = await getMapRenderTime(zoom, center);
     mapRenderedTimes.push(mapRenderedTime);
   }
@@ -79,7 +79,8 @@ const getMapRenderTimeByZoom = async () => {
     const zoomList = [ 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ]
 
     let comment = '<h3>地図レンダリング時間差分</h3>';
-    comment += '<table><tr><th>ズームレベル<</th><th>最新リリースとの差分</th></tr>';
+    comment += '<p><code>master</code> ブランチのスタイルと、この PR のスタイルの地図レンダリング速度を比較した結果を表示します。（レンダリング時間が2秒以上増加した場合テストが失敗します）</p>';
+    comment += '<table><tr><th>ズームレベル</th><th>最新リリースとの差分</th></tr>';
 
     for (let i = 0; i < zoomList.length; i++) {
 
